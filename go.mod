276789c7afe0e9a2fb6892bdfd85164e1e9ea470
Marco Amador <amador.marco@gmail.com>
2020-10-07 13:26:29 +0100

    RegisterEchoServiceHandlerFromEndpoint not working, need to use Serve in  goroutine

--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/golang/protobuf v1.4.2
 	github.com/grpc-ecosystem/grpc-gateway v1.15.0
 	github.com/rs/zerolog v1.20.0
-	golang.org/x/tools v0.0.0-20190828213141-aed303cbaa74
 	google.golang.org/genproto v0.0.0-20201006033701-bcad7cf615f2
 	google.golang.org/grpc v1.32.0
 	google.golang.org/protobuf v1.25.0
--- internal/server.go
+++ internal/server.go
@@ -2,86 +2,106 @@ package echo
 
 import (
 	"context"
+	"flag"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
-	echo "github.com/mfamador/api/v1/internal/gen"
-	"github.com/rs/zerolog/log"
+	"github.com/mfamador/api/v1/internal/gen"
 	"google.golang.org/grpc"
 	"net"
 	"net/http"
-	"strings"
 )
 
 var (
-	ServerPort string
-	SwaggerDir string
-	EndPoint   string
+	ServerPort         string
+	SwaggerDir         string
+	EndPoint           = "8181"
+	grpcServerEndpoint = flag.String("grpc-server-endpoint", "localhost:9090", "gRPC server endpoint")
 )
 
-func init() {
-	ServerPort = "9090"
-}
-
 func Run() error {
-	EndPoint = ":" + ServerPort
-	conn, err := net.Listen("tcp", EndPoint)
-	if err != nil {
-		log.Printf("TCP Listen err:%v\n", err)
-	}
-	srv := newServer(conn)
-	log.Info().Str("gRPC and http listen on: %s\n", ServerPort)
-	if err = srv.Serve(conn); err != nil {
-		log.Printf("Serve: %v\n", err)
-	}
-
-	return err
-}
-
-func newServer(conn net.Listener) *http.Server {
-	grpcServer := newGrpc()
-	gwmux, err := newGateway()
-	if err != nil {
-		panic(err)
-	}
-	mux := http.NewServeMux()
-	mux.Handle("/", gwmux)
-	//mux.HandleFunc("/swagger/", serveSwaggerFile)
-	//serveSwaggerUI(mux)
-
-	return &http.Server{
-		Addr:    EndPoint,
-		Handler: GrpcHandlerFunc(grpcServer, mux),
-	}
-}
-
-func newGrpc() *grpc.Server {
-	server := grpc.NewServer()
-	echo.RegisterEchoServiceServer(server, NewEchoService())
-	return server
-}
-
-func newGateway() (http.Handler, error) {
 	ctx := context.Background()
-	gwmux := runtime.NewServeMux()
-	var dopts []grpc.DialOption
-	dopts = append(dopts, grpc.WithInsecure())
-	if err := echo.RegisterEchoServiceHandlerFromEndpoint(ctx, gwmux, EndPoint, dopts); err != nil {
-		return nil, err
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	grpcServer := grpc.NewServer()
+	echoService := NewEchoService()
+	echo.RegisterEchoServiceServer(grpcServer, echoService)
+
+	grpcLis, err := net.Listen("tcp", "localhost:9090")
+	go grpcServer.Serve(grpcLis)
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err = echo.RegisterEchoServiceHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
+	if err != nil {
+		return err
 	}
-	return gwmux, nil
+
+	// Start HTTP server (and proxy calls to gRPC server endpoint)
+	return http.ListenAndServe(":8081", mux)
 }
 
-func GrpcHandlerFunc(grpcServer *grpc.Server, otherHandler http.Handler) http.Handler {
-	log.Print("GrpcHandlerFunc")
-	if otherHandler == nil {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			grpcServer.ServeHTTP(w, r)
-		})
-	}
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
-			grpcServer.ServeHTTP(w, r)
-		} else {
-			otherHandler.ServeHTTP(w, r)
-		}
-	})
-}
+//
+//func Run() error {
+//	EndPoint = ":" + ServerPort
+//	conn, err := net.Listen("tcp", EndPoint)
+//	if err != nil {
+//		log.Printf("TCP Listen err:%v\n", err)
+//	}
+//	srv := newServer(conn)
+//
+//	log.Info().Str("gRPC and http listen on: %s\n", ServerPort)
+//	if err = srv.Serve(conn); err != nil {
+//		log.Printf("Serve: %v\n", err)
+//	}
+//
+//	return err
+//}
+//
+//func newServer(conn net.Listener) *http.Server {
+//	grpcServer := newGrpc()
+//	gwmux, err := newGateway()
+//	if err != nil {
+//		panic(err)
+//	}
+//	mux := http.NewServeMux()
+//	mux.Handle("/", gwmux)
+//	//mux.HandleFunc("/swagger/", serveSwaggerFile)
+//	//serveSwaggerUI(mux)
+//
+//	return &http.Server{
+//		Addr:    EndPoint,
+//		Handler: GrpcHandlerFunc(grpcServer, mux),
+//	}
+//}
+//
+//func newGrpc() *grpc.Server {
+//	server := grpc.NewServer()
+//	echo.RegisterEchoServiceServer(server, NewEchoService())
+//	return server
+//}
+//
+//func newGateway() (http.Handler, error) {
+//	ctx := context.Background()
+//	gwmux := runtime.NewServeMux()
+//	dialOpts := []grpc.DialOption{grpc.WithInsecure()}
+//	if err := echo.RegisterEchoServiceHandlerFromEndpoint(ctx, gwmux, EndPoint, dialOpts); err != nil {
+//		return nil, err
+//	}
+//	return gwmux, nil
+//}
+//
+//func GrpcHandlerFunc(grpcServer *grpc.Server, otherHandler http.Handler) http.Handler {
+//	log.Print("GrpcHandlerFunc")
+//	if otherHandler == nil {
+//		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//			grpcServer.ServeHTTP(w, r)
+//		})
+//	}
+//	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
+//			grpcServer.ServeHTTP(w, r)
+//		} else {
+//			otherHandler.ServeHTTP(w, r)
+//		}
+//	})
+//}
